66d1dc3d59eb74dc830172dab31b77a62025033f
mojtabamht <mhtmojtaba@gmail.com>
2025-04-02 16:31:37 +0330

    Add files via upload

---
+++ Program (6).cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace میزان_مصرف_برق_یک_خانواده
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int k, h;
+            Console.Write("Please enter the amount consumed : ");
+            k = int.Parse(Console.ReadLine());
+            if (k <= 5)
+            {
+                h = 0;
+            }
+            else if (k <= 20)
+            {
+                h = (k - 5) * 1000;
+            }
+            else if (k > 20)
+            {
+                h = 15000 + ((k - 20) * 5000);
+            }
+            Console.WriteLine(h);
+        }
+    }
+}
